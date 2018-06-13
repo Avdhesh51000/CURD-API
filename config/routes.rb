dad228fa4a5eccf8f4022b1e4cd718bb8df3d790
Avdhesh51000 <avdhesh51000@gmail.com>
2018-06-13 10:46:26 +0530

    Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts 
-  	resources :comments
-  
+  resources :comments
+
+
+	resources :users, only: :create do
+	    collection do
+	      post 'confirm'
+	      post 'login'
+	    end
+	end
+
   
 end
