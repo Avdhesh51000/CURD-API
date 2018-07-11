82afca4d65ca6065b74681f6e4e0cf7f7ca83fd3
Avdhesh51000 <avdhesh51000@gmail.com>
2018-07-11 19:50:41 +0530

    New

---
+++ db/migrate/20180702065815_create_mymodels.rb
@@ -0,0 +1,9 @@
+class CreateMymodels < ActiveRecord::Migration[5.2]
+  def change
+    create_table :mymodels do |t|
+    	t.string :name
+    	t.string :password
+      t.timestamps
+    end
+  end
+end
