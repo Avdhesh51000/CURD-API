965c431c41a3b406860a8c31b4c65120f845a3dc
Avdhesh51000 <avdhesh51000@gmail.com>
2018-07-11 18:36:48 +0530

    Correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+
 class UsersController < ApplicationController
 
 	def create
@@ -28,10 +29,11 @@ end
 def login
 	
   user = User.find_by(email: params[:email].to_s.downcase)
-  
+
+
   if user && user.authenticate(params[:password])
   	      auth_token = JsonWebToken.encode({user_id: user.id})
-    	  render json: {auth_token: auth_token}, status: :ok
+    	  render json:{Loged_in: "You have successfully logedin & Token is : "+ auth_token}, status: :ok
       
   else
     render json: {error: 'Invalid username / password'}, status: :unauthorized
---
+++ test/models/like_test.rb
@@ -0,0 +1,8 @@
+
+require 'test_helper'
+
+class LikeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
